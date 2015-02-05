b6d3570a3b648ef680ad7c6ad0f89d6528b2f458
Spirit_xc <xc1129@gmail.com>
2015-02-05 22:57:27 +0800

    create

---
+++ firststudy/firststudy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  firststudy
+//
+//  Created by 许陈 on 15/2/5.
+//  Copyright (c) 2015年 Spirit_xc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
